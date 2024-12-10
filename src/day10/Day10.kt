da03bb214411bec48260db4f90b7467efa8f9fdc
MABD <mabd.universe@gmail.com>
2024-12-10 12:11:57 +0300

    solved day 10

---
+++ src/day10/Day10.kt
@@ -0,0 +1,155 @@
+package day10
+
+import println
+import readInput
+
+private const val FOLDER_NAME = "day10"
+private const val FILE_NAME = "Day10"
+
+private data class Position(
+    val i: Int,
+    val j: Int
+) {
+
+    fun moveRight() = this.copy(i = i, j = j + 1)
+    fun moveLeft() = this.copy(i = i, j = j - 1)
+    fun moveUp() = this.copy(i = i - 1, j = j)
+    fun moveDown() = this.copy(i = i + 1, j = j)
+
+    fun isValid(width: Int, height: Int): Boolean {
+        return this.i in 0..<width && this.j in 0..< height
+    }
+
+    inline fun ifValid(width: Int, height: Int, block: (Position) -> Unit) {
+        if (isValid(width, height)) block(this)
+    }
+
+}
+
+fun main() {
+
+    fun part1(input: List<String>): Long {
+        fun searchFor9UniquePositions(
+            input: List<List<Int>>,
+            i: Int,
+            j: Int,
+            currentNumber: Int,
+            result: MutableSet<Position>
+        ) {
+            if (i < 0 || i >= input.size || j < 0 || j >= input[i].size) {
+                return
+            }
+            if (input[i][j] == 9) {
+                result.add(Position(i, j))
+                return
+            }
+
+            // check right
+            if (j + 1 < input[i].size && input[i][j+1] == currentNumber + 1) {
+                searchFor9UniquePositions(input, i, j + 1, currentNumber + 1, result)
+            }
+
+            // check left
+            if (j - 1 >= 0 && input[i][j-1] == currentNumber + 1) {
+                searchFor9UniquePositions(input, i, j - 1, currentNumber + 1, result)
+            }
+
+            // check bottom
+            if (i + 1 < input.size && input[i+1][j] == currentNumber + 1) {
+                searchFor9UniquePositions(input, i + 1, j, currentNumber + 1, result)
+            }
+
+            // check top
+            if (i - 1 >= 0 && input[i-1][j] == currentNumber + 1) {
+                searchFor9UniquePositions(input, i - 1, j, currentNumber + 1, result)
+            }
+        }
+
+        val intInput = input.map { line ->
+            line.toCharArray().map {
+                it.toString().toIntOrNull() ?: -1
+            }
+        }
+        var result = 0L
+        val positionsSet = mutableSetOf<Position>()
+
+        for (i in intInput.indices) {
+            for (j in 0 until intInput[i].size) {
+                if (intInput[i][j] == 0) {
+                    positionsSet.clear()
+//                    println("searching from i=$i, j=$j")
+                    searchFor9UniquePositions(intInput, i, j, 0, positionsSet)
+//                    println("positionsSet = $positionsSet\n*********************")
+                    result += positionsSet.size
+                }
+            }
+        }
+        return result
+    }
+
+    fun part2(input: List<String>): Long {
+        fun searchFor9UniquePositions(
+            input: List<List<Int>>,
+            i: Int,
+            j: Int,
+            currentNumber: Int,
+            result: MutableList<Position>
+        ) {
+            if (i < 0 || i >= input.size || j < 0 || j >= input[i].size) {
+                return
+            }
+            if (input[i][j] == 9) {
+                result.add(Position(i, j))
+                return
+            }
+
+            // check right
+            if (j + 1 < input[i].size && input[i][j+1] == currentNumber + 1) {
+                searchFor9UniquePositions(input, i, j + 1, currentNumber + 1, result)
+            }
+
+            // check left
+            if (j - 1 >= 0 && input[i][j-1] == currentNumber + 1) {
+                searchFor9UniquePositions(input, i, j - 1, currentNumber + 1, result)
+            }
+
+            // check bottom
+            if (i + 1 < input.size && input[i+1][j] == currentNumber + 1) {
+                searchFor9UniquePositions(input, i + 1, j, currentNumber + 1, result)
+            }
+
+            // check top
+            if (i - 1 >= 0 && input[i-1][j] == currentNumber + 1) {
+                searchFor9UniquePositions(input, i - 1, j, currentNumber + 1, result)
+            }
+        }
+
+        val intInput = input.map { line ->
+            line.toCharArray().map {
+                it.toString().toIntOrNull() ?: -1
+            }
+        }
+        var result = 0L
+        val positionsSet = mutableListOf<Position>()
+
+        for (i in intInput.indices) {
+            for (j in 0 until intInput[i].size) {
+                if (intInput[i][j] == 0) {
+                    positionsSet.clear()
+                    searchFor9UniquePositions(intInput, i, j, 0, positionsSet)
+                    result += positionsSet.size
+                }
+            }
+        }
+        return result
+    }
+
+    val testInput = readInput(FOLDER_NAME, "${FILE_NAME}_test")
+//    check(part1(testInput) == 36L)
+    check(part2(testInput) == 81L)
+
+    val input = readInput(FOLDER_NAME, FILE_NAME)
+//    part1(input).println()
+    part2(input).println()
+
+}
