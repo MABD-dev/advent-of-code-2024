29a151167a50643aa7312295e2af53c6e7e2a453
MABD <mabd.universe@gmail.com>
2024-12-11 18:19:40 +0300

    Finished day11

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,108 +1,57 @@
 
-fun String.toIntWithoutZeros(): Int = this.toInt()
-
-private fun calculateShit(
-    stones: MutableList<Long>,
-) {
-    var i = 0
-    while (i < stones.size) {
-        if (stones[i] == 0L) {
-            stones[i++] = 1L
-        } else if (stones[i].toString().length % 2 == 0) {
-            val str = stones[i].toString()
-            val left = str.substring(0, str.length/2).toLong()
-            val right = str.substring(str.length/2).toLong()
-            stones.add(i, left)
-            stones[++i] = right
-            i++
-        } else {
-            stones[i++] *= 2024L
-        }
-    }
+private fun formatInput(input: List<String>): List<Long> {
+    return input[0].split(" ").map { it.toLong() }
 }
 
-private fun seeHowMany(numbers: MutableList<Long>, steps: Int, cache: MutableMap<Long, MutableList<Long>>): Long {
-//    if (cache.containsKey(number) && cache[number]!!.size > steps) {
-//        println("cache hit, number=$number, steps=$steps")
-//        return cache[number]!![steps]
-//    }
-
-    var result = 0L
-
-//    repeat(steps) { step ->
-//
-//        // check if any of the numbers is already cached, if yes, remove it from numbers list + add size to result
-//        for (i in numbers.indices) {
-//            if (cache.containsKey(numbers[i] && ))
-//        }
-//
-//
-//        var i = 0
-//        while (i < numbers.size) {
-//            if (numbers[i] == 0L) {
-//                numbers[i++] = 1L
-//            } else if (numbers[i].toString().length % 2 == 0) {
-//                val str = numbers[i].toString()
-//                val left = str.substring(0, str.length/2).toLong()
-//                val right = str.substring(str.length/2).toLong()
-//                numbers.add(i, left)
-//                numbers[++i] = right
-//                i++
-//            } else {
-//                numbers[i++] *= 2024L
-//            }
-//        }
-//        cache[numbers]!!.add(numbers.size.toLong())
-//    }
-    return result
-}
-
-private fun solve(
+private fun blink(
     stones: MutableList<Long>,
     steps: Int
 ): Long {
+    var map = mutableMapOf<Long, Long>()
+    stones.forEach { num -> map[num] = map.getOrDefault(num, 0L) + 1 }
 
-//    val cache = mutableMapOf<Long, MutableList<Long>>()
-//    seeHowMany(0, steps, cache)
-//    seeHowMany(1, steps, cache)
-//    seeHowMany(2024, steps, cache)
-//
-//    var stoneIndex = 0
-//    while (stoneIndex < stones.size) {
-//        val number = stones[stoneIndex]
-//        seeHowMany(number, steps, cache)
-//
-//        stoneIndex++
-//    }
-//
-//    cache.keys.forEach { key ->
-//        println("$key -> ${cache[key]}")
-//    }
+    repeat(steps) {
+        val newMap = mutableMapOf<Long, Long>()
 
-    return 0L
+        map.forEach { (number, oldCount) ->
+            if (number == 0L) {
+                newMap[1L] = newMap.getOrDefault(1L, 0L) + oldCount
+            } else if (number.toString().length % 2 == 0) {
+                val numberStr = number.toString()
+                val leftValue = numberStr.substring(0, numberStr.length/2).toLong()
+                val rightValue = numberStr.substring(numberStr.length/2).toLong()
+
+                newMap[leftValue] = newMap.getOrDefault(leftValue, 0L) + oldCount
+                newMap[rightValue] = newMap.getOrDefault(rightValue, 0L) + oldCount
+            } else {
+                val newNumber = number * 2024
+                newMap[newNumber] = newMap.getOrDefault(newNumber, 0L) + oldCount
+            }
+            map = newMap
+        }
+    }
+    return map.values.sum()
 }
 
 fun main() {
 
     fun part1(input: List<String>): Long {
-        val stones = input[0].split(" ").map { it.toLong() }.toMutableList()
-        return solve(stones, 25)
+        val stones = formatInput(input).toMutableList()
+        return blink(stones, 25)
     }
 
     fun part2(input: List<String>): Long {
-        val stones = input[0].split(" ").map { it.toLong() }.toMutableList()
-        return solve(stones, 75)
+        val stones = formatInput(input).toMutableList()
+        return blink(stones, 75)
     }
 
     val testInput = readTestInput()
-    part1(testInput)
-//    part2(testInput)
 //    check(part1(testInput) == 55312L)
 //    check(part2(testInput) == 0L)
 
     val input = readInput()
-//    part1(input).println()
-//    part2(input).println()
+    part1(input).println() // 204022
+    part2(input).println() // 241651071960597
 
 
 }
